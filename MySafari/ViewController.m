22a452096a0a02a5322ef613f21ed24634eb4550
Andrew Nguyen <anguy080@ucr.edu>
2015-09-17 13:08:10 -0700

    done

--- MySafari/ViewController.m
+++ MySafari/ViewController.m
@@ -21,26 +21,38 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     self.backButton.enabled = NO;
     self.forwardButton.enabled = NO;
     [self.webView.scrollView setDelegate:self];
 
-    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.apple.com"]]];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.apple.com"]]];
 
 }
 
+- (void)checkForwardAndBackButtons {
+
+    if (self.webView.canGoBack) {
+        self.backButton.enabled = YES;
+    } else {
+        self.backButton.enabled = NO;
+    }
+
+    if (self.webView.canGoForward) {
+        self.forwardButton.enabled = YES;
+    } else {
+        self.forwardButton.enabled = NO;
+    }
+}
+
+#pragma mark - TextField Delagate Methods
 -(BOOL)textFieldShouldReturn:(UITextField *)textField {
     NSURL *url;
-    if ([textField.text containsString:@"http://www."]) {
+    if ([textField.text containsString:@"https://www."]) {
         url = [NSURL URLWithString:textField.text];
-
-    }
-    else if ([textField.text containsString:@"www."]) {
-        url = [NSURL URLWithString:[NSString stringWithFormat:@"http://%@", textField.text]];
-    }
-    else {
-        url = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.%@",textField.text]];
+    } else if ([textField.text containsString:@"www."]) {
+        url = [NSURL URLWithString:[NSString stringWithFormat:@"https://%@", textField.text]];
+    } else {
+        url = [NSURL URLWithString:[NSString stringWithFormat:@"https://www.%@",textField.text]];
     }
 
     [self.webView loadRequest:[NSURLRequest requestWithURL:url]];
@@ -48,28 +60,20 @@
     return NO;
 }
 
+#pragma mark - WebView Delagate Methods
 -(void)webViewDidStartLoad:(UIWebView *)webView {
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    if (webView.canGoBack) {
-        self.backButton.enabled = YES;
-    }
-    else {
-        self.backButton.enabled = NO;
-    }
-    if (webView.canGoForward) {
-        self.forwardButton.enabled = YES;
-    }
-    else {
-        self.forwardButton.enabled = NO;
-    }
+    [self checkForwardAndBackButtons];
 }
 
 -(void)webViewDidFinishLoad:(UIWebView *)webView {
     [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
     self.urlTextField.text = webView.request.URL.absoluteString;
     self.title = [webView stringByEvaluatingJavaScriptFromString:@"document.title"];
+    [self checkForwardAndBackButtons];
 }
 
+#pragma mark - Button Presses
 - (IBAction)onBackButtonPressed:(UIButton *)sender {
     [self.webView goBack];
 }
@@ -84,8 +88,15 @@
     [self.webView reload];
 }
 - (IBAction)onPlusButtonPressed:(UIButton *)sender {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"New Feauture!" message:@"Comming soon!" preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"New Feature!"
+                                                                   message:@"Coming soon!"
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK"
+                                                       style:UIAlertActionStyleDefault
+                                                     handler:^(UIAlertAction *action) {
+
         [self dismissViewControllerAnimated:YES completion:nil];
     }];
 
@@ -93,15 +104,16 @@
     [self presentViewController:alert animated:YES completion:nil];
 }
 
+#pragma mark - ScrollView Delagate Methods
 -(void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
     self.newPoint = scrollView.contentOffset;
 }
 
 -(void)scrollViewDidScroll:(UIScrollView *)scrollView {
+
     if (scrollView.contentOffset.y > self.newPoint.y) {
         self.urlTextField.hidden = YES;
-    }
-    else {
+    } else {
         self.urlTextField.hidden = NO;
     }
 }
